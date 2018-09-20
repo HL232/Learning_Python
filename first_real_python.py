2d9b0825b1a87c59ed8073b1716ad95091c63202
Howard <howard.li@duke.edu>
2018-09-20 15:57:32 -0400

    Added unit tests for first python file

--- first_real_python.py
+++ first_real_python.py
@@ -1,10 +1,9 @@
-def main():
-	a=add_three(3, 5, 1)
-	divide_three(a)
+#def main():
+#	a=add_three(3, 5, 1)
+#	divide_three(a)
 
 def add_three(v1,v2,v3):
 	p= v1+v2+v3
-	print(p)
 	return p
 
 def divide_three(v1):
@@ -12,8 +11,8 @@ def divide_three(v1):
 	:param v1: number one
 	:returns: product number
 	"""
-	p=v1/3
-	print(p)
+	result =v1/3
+	return result
 
-if __name__ == "__main__":
-	main()
+#if __name__ == "__main__":
+#	main()
